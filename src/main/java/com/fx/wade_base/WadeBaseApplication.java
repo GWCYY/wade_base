3d8fe5ee99de23c4950e2aeb44ab3d33067b8a40
wade <15155065624@163.com>
2022-11-13 12:32:10 +0800

    新项目

---
+++ src/main/java/com/fx/wade_base/WadeBaseApplication.java
@@ -0,0 +1,13 @@
+package com.fx.wade_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WadeBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WadeBaseApplication.class, args);
+    }
+
+}
